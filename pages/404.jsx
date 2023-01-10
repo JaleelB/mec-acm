b0ebb15fdf542296dba61d1614d88f2108019812
Jaleel Bennett <>
2023-01-10 15:13:37 -0500

    added infninte scrolling banner

---
+++ pages/404.jsx
@@ -0,0 +1,71 @@
+import { Box, Flex, keyframes } from '@chakra-ui/react'
+import React from 'react'
+import { Title } from  '../components'
+
+const animation = keyframes`
+   0% {
+    transform: translate3d(0%, 0, 0);
+  }
+  100% {
+    transform: translate3d(-50%, 0, 0);
+  }
+`
+
+const scrollingAnimation = `${animation} linear infinite 12s`
+
+const Error = () => {
+  return (
+
+    <Flex
+        w="100vw" h="100vh" 
+        bgColor="bgDark" color="textLight"
+        align="center" justify="center"
+    >
+
+        <Box 
+            w="100%" overflow="hidden"
+        >
+            <Box    
+                w="max-content" gap={{base: "5vw", smTablet: "3.2vw"}}
+                display="flex" whiteSpace="nowrap" 
+                animation={scrollingAnimation} overflow="hidden"
+                animationDelay="-2.5"
+            >
+                <Box display="flex" gap={{base: "5vw", smTablet: "3.2vw"}}>
+                    {
+                        [1,2,3, 4, 5, 6, 7, 8].map(cardCount => {
+                            return (
+                                
+                                <Flex 
+                                    key={cardCount}
+                                    w="max-content" 
+                                >
+                                    <Title>404 page</Title>   
+                                </Flex>
+                            )
+                        })
+                    }  
+                </Box>
+                <Box display="flex" gap={{base: "5vw", smTablet: "3.2vw"}}>
+                    {
+                        [1,2,3, 4, 5, 6, 7, 8].map(cardCount => {
+                            return (
+                                
+                                <Flex 
+                                    key={cardCount}
+                                    w="max-content" 
+                                >
+                                    <Title>404 page</Title>   
+                                </Flex>
+                            )
+                        })
+                    } 
+                </Box>
+            </Box>
+        </Box>
+
+    </Flex>
+  )
+}
+
+export default Error
