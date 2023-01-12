716835a27536ac318a5498f7c1da08a97cfd58af
Jaleel Bennett <>
2023-01-11 20:14:23 -0500

    updated gap in layout on extra large screens

--- components/Subtitle.jsx
+++ components/Subtitle.jsx
@@ -6,7 +6,8 @@ const Subtitle = ({children}) => {
     <Heading as="h3"
       fontSize={{base: "29px", sm: "36px", md:"50px", lg:"65px", xlDesktop: '80px'}}
       lineHeight={{base: "100%", md: "107%"}}
-      maxW={{lg: '95%', mdDesktop: '87.5%', xlDesktop:'75%'}}
+      // maxW={{lg: '95%', mdDesktop: '87.5%', xlDesktop:'75%'}}
+      maxW={{lg:'87.5%%', xlDesktop:'75%'}}
     >
         {children}
     </Heading>
