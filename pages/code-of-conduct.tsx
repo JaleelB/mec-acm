eee7598c1df8fc19789d64b264f74a3200656617
Jaleel Bennett <>
2023-01-14 00:17:49 -0500

    added animation to body text

--- pages/code-of-conduct.tsx
+++ pages/code-of-conduct.tsx
@@ -42,46 +42,56 @@ const CodeofConduct = () => {
                 >
                     <Box w="100%" h="1px" bg="lightGrey" my={{base: '3rem', lgTablet: '6rem', mdDesktop: '9rem'}}/>
                 </motion.div>
+                
+                <motion.div
+                    initial={{ opacity: 0, y: 90 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{
+                        type: "easeInOut",
+                        duration: .9,
+                        delay: 2.2,
+                    }}
+                >
+                    <Flex gap={{base: '2rem', lgTablet: '2.5rem', mdDesktop: '3.5rem'}} direction="column">
+                        <Text>
+                            To that end, we have established this code of conduct to set clear expectations for how we expect our community members to behave. By participating in our community, 
+                            you agree to abide by this code of conduct and to treat others with respect and kindness.
+                        </Text>
 
-                <Flex gap={{base: '2rem', lgTablet: '2.5rem', mdDesktop: '3.5rem'}} direction="column">
-                    <Text>
-                        To that end, we have established this code of conduct to set clear expectations for how we expect our community members to behave. By participating in our community, 
-                        you agree to abide by this code of conduct and to treat others with respect and kindness.
-                    </Text>
+                        <List spacing={3}>
+                            <Text mb="1rem">We will not tolerate any form of discrimination, harassment, or bullying. This includes, but is not limited to:</Text>
+                            {
+                                formsOfDiscrimination.map(form => {
+                                    return (
+                                        <ListItem 
+                                            key={form} 
+                                            display="flex"
+                                            gap=".7rem"
+                                            pl={{base: '1rem', lgTablet: '2rem', mdDesktop: '3rem'}}
+                                        >
+                                            <ListIcon as={CheckCircleIcon} color='colorBlue' />
+                                            <Text>{form}</Text>
+                                        </ListItem>
+                                    )
+                                })
+                            }
+                            
+                        </List>
 
-                    <List spacing={3}>
-                        <Text mb="1rem">We will not tolerate any form of discrimination, harassment, or bullying. This includes, but is not limited to:</Text>
-                        {
-                            formsOfDiscrimination.map(form => {
-                                return (
-                                    <ListItem 
-                                        key={form} 
-                                        display="flex"
-                                        gap=".7rem"
-                                        pl={{base: '1rem', lgTablet: '2rem', mdDesktop: '3rem'}}
-                                    >
-                                        <ListIcon as={CheckCircleIcon} color='colorBlue' />
-                                        <Text>{form}</Text>
-                                    </ListItem>
-                                )
-                            })
-                        }
-                        
-                    </List>
+                        <Text>
+                            We encourage open and respectful communication, and expect all community members to listen to and consider the viewpoints of others. It is okay 
+                            to disagree, but it is not okay to disrespect or belittle others for their opinions. We also expect all community members to respect the intellectual 
+                            property of others. This includes not sharing or distributing copyrighted material without permission.
+                        </Text>
 
-                    <Text>
-                        We encourage open and respectful communication, and expect all community members to listen to and consider the viewpoints of others. It is okay 
-                        to disagree, but it is not okay to disrespect or belittle others for their opinions. We also expect all community members to respect the intellectual 
-                        property of others. This includes not sharing or distributing copyrighted material without permission.
-                    </Text>
+                        <Text>
+                            If you witness or experience any behavior that violates this code of conduct, please report it to a club administrator immediately. We take 
+                            all reports seriously and will take appropriate action to address any violations. We are committed to creating a positive and welcoming environment 
+                            for all members of our community. Thank you for your cooperation in helping us achieve this goal.
+                        </Text>
 
-                    <Text>
-                        If you witness or experience any behavior that violates this code of conduct, please report it to a club administrator immediately. We take 
-                        all reports seriously and will take appropriate action to address any violations. We are committed to creating a positive and welcoming environment 
-                        for all members of our community. Thank you for your cooperation in helping us achieve this goal.
-                    </Text>
-
-                </Flex>
+                    </Flex>
+                </motion.div>
                 
                 <Box 
                     className='floor-space'
