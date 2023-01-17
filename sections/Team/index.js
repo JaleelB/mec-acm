dfeef12b46c1e435751c8a3581dab2f26e15258f
Jaleel Bennett <>
2023-01-16 23:56:31 -0500

    added team page

---
+++ sections/Team/index.js
@@ -0,0 +1,4 @@
+export {default as Hero} from './Hero'
+export {default as Banner} from './Banner'
+export {default as ElectiveBody} from './ElectiveBody'
+export {default as KnowMore} from './KnowMore'
