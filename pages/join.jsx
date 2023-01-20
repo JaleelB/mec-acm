fe1a5d294207512110e88a538b2ed81688bbe542
Jaleel Bennett <>
2023-01-19 22:55:10 -0500

    added join page animations

--- pages/join.jsx
+++ pages/join.jsx
@@ -1,6 +1,7 @@
 import { Box, Flex, Stack, Text } from '@chakra-ui/react'
+import { motion } from 'framer-motion'
 import React from 'react'
-import { ResponsiveSection, RoundButton, TextInput, Title } from '../components'
+import { HeroAnimationWrapper, ResponsiveSection, RoundButton, TextInput, Title } from '../components'
 import { Layout } from '../layout'
 
 const Join = () => {
@@ -12,11 +13,11 @@ const Join = () => {
             display={{base: 'block', lg: 'none'}}
             pt={{base: '20vw', lgTablet: '5vw', lg: 0}}
         />
-        <Box>
+        <HeroAnimationWrapper>
           <Title>Hey there!</Title>
           <Title>Wanna join the club?</Title>
           <Title>You will have to fill out a form</Title>
-        </Box>
+        </HeroAnimationWrapper>
 
         <Flex 
           direction={{base: "column", lg: "row"}}
@@ -24,20 +25,41 @@ const Join = () => {
           mt={{base: '5rem', lgTablet:'7.5rem', lgDesktop:'wrap2Md'}}
         >
           <Box flex=" 0 0 45%" >
-            <Text w={{base: "100%", md: "75%"}}>
-              You do not need prior experience to join—just a passion for learning! 
-              The MEC ACM is a club dedicated to exploring the world of computer science. 
-              To that extent, we are always looking for new members to embark on this journey. 
-              You do not have to be an expert on anything; just bring your curiosity and enthusiasm 
-              for learning!
-            </Text>   
+            <motion.div
+              initial={{ opacity: 0, y: 90 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{
+                  type: "easeInOut",
+                  duration: .9,
+                  delay: 1.2,
+              }}
+            >
+              <Text w={{base: "100%", md: "75%"}}>
+                You do not need prior experience to join—just a passion for learning! 
+                The MEC ACM is a club dedicated to exploring the world of computer science. 
+                To that extent, we are always looking for new members to embark on this journey. 
+                You do not have to be an expert on anything; just bring your curiosity and enthusiasm 
+                for learning!
+              </Text>   
+            </motion.div>
           </Box>
-          <Stack flex="0 0 45%" spacing="1rem" mt={{base: '5rem', lgTablet:0}}>
-              <TextInput label="What is your name?" ph="John/Jane Doe"/>
-              <TextInput label="What is your preferred email to receive club emails?" ph="studentname@personal.com"/>
-              <TextInput label="What is your current year in school?" ph="Freshman, Sophomore..." />
-              <TextInput label="What is your major?" ph="Computer Science, Biology..."/>
-          </Stack>
+
+          <motion.div
+              initial={{ opacity: 0, y: 90 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{
+                  type: "easeInOut",
+                  duration: .9,
+                  delay: 2.2,
+              }}
+          >
+            <Stack flex="0 0 45%" spacing="1rem" mt={{base: '5rem', lgTablet:0}}>
+                <TextInput label="What is your name?" ph="John/Jane Doe"/>
+                <TextInput label="What is your preferred email to receive club emails?" ph="studentname@personal.com"/>
+                <TextInput label="What is your current year in school?" ph="Freshman, Sophomore..." />
+                <TextInput label="What is your major?" ph="Computer Science, Biology..."/>
+            </Stack>
+          </motion.div>
         </Flex>
 
         <Flex 
