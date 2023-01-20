008dcd3ed2f76dd7762fe482424e55eb6edc7b79
Jaleel Bennett <>
2023-01-19 20:55:52 -0500

    latest changes

--- components/BodyParagraph.jsx
+++ components/BodyParagraph.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const BodyParagraph = ({children}) => {
   return (
     <Text
-        maxW={{base:'600px', lgTablet: '65%',  xlDesktop: '50%' }}
+        maxW={{base:'600px', lgTablet: '50%' }}
     >
         {children}
     </Text>
