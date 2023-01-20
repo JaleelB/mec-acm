6856b7a24bd633539fd87a50c3986b8d2859ab68
Jaleel Bennett <>
2023-01-19 22:42:29 -0500

    added dark mode prop to layout wrapper

--- components/Input.jsx
+++ components/Input.jsx
@@ -5,15 +5,17 @@ const TextInput = ({label, ph}) => {
   return (
     <Box w="100%" py={4}>
         <Flex mb="1rem">
-            <Text as="label">{label}</Text>
-            <Box as="span" ml=".2rem" color="red.400">*</Box>
+          <Text as="label">{label}</Text>
+          <Box as="span" ml=".2rem" color="red.400">*</Box>
         </Flex>
         <Input 
             fontSize="inherit"
             variant="flushed" 
             w="100%" size="lg"
             placeholder={ph}
+            _placeholder={{ color: 'colorLight' }}
             pb="2rem" h={{mdDesktop: "75px"}}
+            borderColor="colorLight"
         />
     </Box>
   )
