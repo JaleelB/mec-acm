032e5b3f584b987ec5a7e0f03a4216e69e527b2d
Jaleel Bennett <>
2023-01-20 23:38:00 -0500

    added react hook form to form layout, added error toast, and updated input component

--- components/Input.jsx
+++ components/Input.jsx
@@ -1,22 +1,38 @@
-import { Box, Flex, Text, Input } from '@chakra-ui/react'
+import { Box, Flex, Input, FormLabel, FormErrorMessage } from '@chakra-ui/react'
 import React from 'react'
 
-const TextInput = ({label, ph}) => {
+const TextInput = ({label, ph, id, ref, errors, register}) => {
   return (
     <Box w="100%" py={4}>
         <Flex mb="1rem">
-          <Text as="label">{label}</Text>
+          <FormLabel htmlFor={id}>{label}</FormLabel>
           <Box as="span" ml=".2rem" color="red.400">*</Box>
         </Flex>
+
         <Input 
-            fontSize="inherit"
-            variant="flushed" 
-            w="100%" size="lg"
-            placeholder={ph}
-            _placeholder={{ color: 'colorLight' }}
-            pb="2rem" h={{mdDesktop: "75px"}}
-            borderColor="colorLight"
+          ref={ref}
+          id={id}
+          type="text"
+          fontSize="inherit"
+          variant="flushed" 
+          w="100%" size="lg"
+          placeholder={ph}
+          _placeholder={{ color: 'colorLight' }}
+          pb="2rem" h={{mdDesktop: "75px"}}
+          borderColor="colorLight"
+          {...register(id, {
+            required: 'This is required',
+            minLength: 1,
+            pattern: id == 'year' && /^(freshman|Freshman|sophomore|Sophomore|junior|Junior|senior|Senior)$/,
+            shouldFocusError: true,
+          })}
+          aria-invalid={errors.id ? "true" : "false"} 
         />
+
+        <FormErrorMessage>
+          {errors.id && errors.id?.message}
+        </FormErrorMessage>
+
     </Box>
   )
 }
--- pages/join.jsx
+++ pages/join.jsx
@@ -1,11 +1,146 @@
-import { Box, Flex, Stack, Text } from '@chakra-ui/react'
+import { 
+  Box, Flex, Stack, Text,
+  FormControl, Button, useToast
+} from '@chakra-ui/react'
+
 import { motion } from 'framer-motion'
-import React from 'react'
-import { HeroAnimationWrapper, ResponsiveSection, RoundButton, TextInput, Title } from '../components'
+import React, { useState, useRef } from 'react'
+import { HeroAnimationWrapper, ResponsiveSection, TextInput, Title } from '../components'
 import { Layout } from '../layout'
+import { useForm } from 'react-hook-form'
+
+
+const RoundButton = (props) => {
+
+  return (
+    <Button
+        isLoading={props.isSubmitting} 
+        role="group" type='submit'
+        width="clamp(144px, 12vw, 270px)" height="clamp(144px, 12vw, 270px)"
+        bg={props.state ? "colorBlue" : "bgDark"} color="textLight"
+        textTransform="capitalize" fontWeight="400"
+        transition="transform ease-in 300ms"
+        justify="center" align="center"
+        borderRadius="100%"
+        _hover={{
+            backgroundColor: 'colorBlue',
+            transform: 'scale(1.1)'
+        }}
+        onClick={props.handleEmptyFields}
+        // onClick={() => {
+      
+        //   if()
+        //     toast({
+        //       title: 'Empty Inputs',
+        //       description: `Fill out all inputs to continue`,
+        //       status: `error`,
+        //       duration: 5000,
+        //       isClosable: true,
+        //       position: 'top-right',
+        //     })
+        // }}
+    >
+        <Flex 
+            justify="center" direction="column"
+            overflow="hidden" height="20px"
+          >
+            <Text 
+              as="div" height="100%"
+              style={{transformStyle: "preserve-3d"}}
+              transition="transform 300ms ease" 
+              transform="translateY(50%)"
+              display="flex" justifyContent="center" alignItems="center"
+              _groupHover={{      
+                transform: "translateY(-100%)"                                  
+              }}
+            >
+              {props.text}
+            </Text>
+            <Text 
+              as="div" height="100%"
+              style={{transformStyle: "preserve-3d"}}
+              transition="transform 300ms ease" 
+              transform="translateY(50%)"
+              _groupHover={{
+                transform:"translateY(-100%)"
+              }}
+            >
+              {props.text}
+            </Text>
+        </Flex>
+    </Button>
+  )
+}
+
 
 const Join = () => {
-  return (
+
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting },
+  } = useForm()
+
+  const toast = useToast();
+  const nameRef = useRef(null);
+  const emailRef = useRef(null);
+  const yearRef = useRef(null);
+  const majorRef = useRef(null);
+  const [error, setError] = useState(false);
+
+  const addMember = async(e) => {
+        
+    const member = {
+        name: nameRef.current?.value,
+        email: emailRef.current?.value,
+        year: yearRef.current?.value,
+        major: majorRef.current?.value,
+        date: `${date.getMonth() < 10 ? `0${date.getMonth() + 1}` : date.getMonth()+1}/${date.getDate()}/${date.getFullYear()}`
+    };
+
+    try{
+        const response = await axios.post('/api/signup', {
+          ...member
+        })
+
+        if(response.data.success){
+            const timer = setTimeout(() => setSuccess(true), 2000);
+            return () => clearTimeout(timer);
+        }
+    }
+    catch(e){
+        setErrorMessage(e.message || 'Something went wrong with signing up');
+    }
+
+};
+
+  const onSubmit = (values) => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        addMember()
+        resolve()
+      }, 2000)
+    })
+  }
+
+  const handleEmptyFields = () => {
+    if(!nameRef.current?.value || !emailRef.current?.value || !majorRef.current?.value || !yearRef.current?.value){
+      toast({
+        title: 'Empty Inputs',
+        description: "Fill out all input fields.",
+        status: 'error',
+        isClosable: false,
+        duration: 10000,
+        position: 'top-right',
+        containerStyle: {
+          fontSize: "clamp(16px, 1.2vw, 20px)"
+        },
+      })
+    }
+  }
+
+  return (  
+
     <Layout dark noFooter>
       <ResponsiveSection>
         <Box 
@@ -50,23 +185,48 @@ const Join = () => {
               transition={{
                   type: "easeInOut",
                   duration: .9,
-                  delay: 2.2,
+                  delay: 1.2,
               }}
           >
-            <Stack flex="0 0 45%" spacing="1rem" mt={{base: '5rem', lgTablet:0}}>
-                <TextInput label="What is your name?" ph="John/Jane Doe"/>
-                <TextInput label="What is your preferred email to receive club emails?" ph="studentname@personal.com"/>
-                <TextInput label="What is your current year in school?" ph="Freshman, Sophomore..." />
-                <TextInput label="What is your major?" ph="Computer Science, Biology..."/>
+            <Stack 
+              as="form" 
+              flex="0 0 45%" 
+              spacing="1rem" 
+              mt={{base: '5rem', lgTablet:0}}
+              onSubmit={handleSubmit(onSubmit)}
+            >
+                <FormControl isInvalid={error}>
+                  <TextInput 
+                    label="What is your name?" ph="John/Jane Doe" id="name" 
+                    ref={nameRef} errors={errors}  register={register}
+                  />
+                  <TextInput
+                     label="What is your preferred email to receive club emails?" ph="studentname@personal.com" 
+                     id="email" ref={emailRef} errors={errors}  register={register}
+                    />
+                  <TextInput 
+                    label="What is your current year in school?" ph="Freshman, Sophomore..." 
+                    id="year" ref={yearRef} errors={errors} register={register}
+                  />
+                  <TextInput 
+                    label="What is your major?" ph="Computer Science, Biology..." 
+                    id="major" ref={majorRef} errors={errors}  register={register}
+                  />
+                </FormControl>
+
+                <Flex 
+                  w="100%" justify="end"
+                  pt={{base: '5rem', lgTablet:'7.5rem', lgDesktop:'wrap2Md'}}
+                >
+                  <RoundButton 
+                    text="submit!" 
+                    state 
+                    handleEmptyFields={handleEmptyFields}
+                    isSubmitting={isSubmitting}
+                  />
+                </Flex>
             </Stack>
           </motion.div>
-        </Flex>
-
-        <Flex 
-          w="100%" justify="end"
-          mt={{base: '5rem', lgTablet:'7.5rem', lgDesktop:'wrap2Md'}}
-        >
-          <RoundButton text="submit!" state/>
         </Flex>
 
         <Box 
