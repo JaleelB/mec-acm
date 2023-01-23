91874cf655b9ed20e6b032fc3f15ca5a6f9fd5cb
Jaleel Bennett <>
2023-01-23 00:57:25 -0500

    added ceiling and floor padding space to body, and added see more button

--- pages/resources.jsx
+++ pages/resources.jsx
@@ -1,5 +1,5 @@
 import { ArrowForwardIcon } from '@chakra-ui/icons'
-import { Box, Flex, Grid, GridItem, Heading, Text } from '@chakra-ui/react'
+import { Box, Button, Flex, Grid, GridItem, Heading, Text } from '@chakra-ui/react'
 import Link from 'next/link'
 import React from 'react'
 import { ResponsiveSection, SectionHeader, Title } from '../components'
@@ -19,6 +19,11 @@ const Resources = () => {
     return (
         <Layout>
             <ResponsiveSection>
+                <Box 
+                    className='ceiling-space'
+                    display={{base: 'block', lg: 'none'}}
+                    pt={{base: '20vw', lgTablet: '5vw', lg: 0}}
+                />
                 <SectionHeader>Resources</SectionHeader>
                 <Box maxW="1700px"> 
                     <Title>
@@ -99,6 +104,58 @@ const Resources = () => {
                     }
                     
                 </Grid>
+
+                <Button
+                    role="group"
+                    bg="bgDark" color="textLight"
+                    textTransform="capitalize" fontWeight="400"
+                    fontSize={{base: 'md', lgDesktop: '20px', xlDesktop: 'xl'}}
+                    transition="transform ease-in 300ms"
+                    justify="center" align="center"
+                    w="max-content" mx="auto" display="flex"
+                    py={{base: 7, mdDesktop: 9}} px={{base: 8, tablet:12}}
+                    mt={{base: '5rem', lgTablet:'7.5rem', lgDesktop:'wrap2Md'}}
+                    borderRadius="100px"
+                    _hover={{
+                        backgroundColor: 'colorBlue'
+                    }}
+                >
+                    <Flex
+                        justify="center" direction="column"
+                        overflow="hidden" height="20px"
+                    >
+                        <Flex 
+                            height="100%"
+                            style={{transformStyle: "preserve-3d"}}
+                            transition="transform 300ms ease" 
+                            transform="translateY(50%)"
+                            align="center"
+                            _groupHover={{      
+                                transform: "translateY(-50%)"                                  
+                            }}
+                        >
+                            See more
+                        </Flex>
+                        <Flex
+                            height="100%"
+                            style={{transformStyle: "preserve-3d"}}
+                            transition="transform 300ms ease" 
+                            transform="translateY(50%)"
+                            align="center"
+                            _groupHover={{
+                                transform:"translateY(-50%)"
+                            }}
+                        >
+                            See more
+                        </Flex>
+                    </Flex>
+                </Button>
+
+                <Box 
+                    className='floor-space'
+                    display={{base: 'block', lg: 'none'}}
+                    pt={{base: '20vw', lgTablet: '5vw', lg: 0}}
+                />
                 
             </ResponsiveSection>
         </Layout>
