629e9c1e9bced20946fd57c46ef3a891b071ff8d
Jaleel Bennett <>
2023-01-22 23:58:11 -0500

    added resources filter bar

--- pages/resources.jsx
+++ pages/resources.jsx
@@ -1,4 +1,4 @@
-import { Box, Stack } from '@chakra-ui/react'
+import { Box, Flex } from '@chakra-ui/react'
 import React from 'react'
 import { ResponsiveSection, SectionHeader, Title } from '../components'
 import { Layout } from '../layout'
@@ -13,6 +13,31 @@ const Resources = () => {
                     Discover helpful resources for your studies and projects.
                 </Title>
             </Box>
+
+            <Flex as="ul"
+                listStyleType="none" p={0}
+                w='100%' justify="space-between"
+                mt={{base: '5rem', lgTablet:'7.5rem', lgDesktop:'wrap2Md'}}
+            >
+                {
+                    [
+                        'All', 'Courses, Tutorials, Bootcamps', 'Youtube Channels', 
+                        'Interview Prep', 'Web Development', 'Web Design'
+                    ].map(filter => {
+                        return (
+                            <Box aas="li" key={filter}
+                                cursor="pointer"
+                                _hover={{
+                                    color: "colorBlue"
+                                }}
+                            >
+                                {filter}
+                            </Box>
+                        )
+                    })
+                }
+                
+            </Flex>
             
         </ResponsiveSection>
     </Layout>
