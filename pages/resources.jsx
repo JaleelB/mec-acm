c23bfc67b9703f807f69e395f6c44e1787694569
Jaleel Bennett <>
2023-01-28 20:40:14 -0500

    added animtions to resources page

--- pages/resources.jsx
+++ pages/resources.jsx
@@ -1,9 +1,7 @@
-import { ArrowForwardIcon } from '@chakra-ui/icons'
-import { Box, Button, Flex, Grid, GridItem, Heading, Tab, TabList, Tabs, Text } from '@chakra-ui/react'
+import { Box, Flex, GridItem } from '@chakra-ui/react'
 import axios from 'axios'
-import Link from 'next/link'
 import React, {useEffect, useState} from 'react'
-import { Filters, Resource, ResponsiveSection, SectionHeader, TabContent, Title } from '../components'
+import { Resource, ResponsiveSection, SectionHeader, TabContent, Title, HeroAnimationWrapper } from '../components'
 import { Layout } from '../layout'
 
 
@@ -39,81 +37,83 @@ const Resources = ({ resources }) => {
 
     return (
         <Layout>
-            <ResponsiveSection>
-                <Box 
-                    className='ceiling-space'
-                    display={{base: 'block', lg: 'none'}}
-                    pt={{base: '20vw', lgTablet: '5vw', lg: 0}}
-                />
+            <HeroAnimationWrapper>
+                <ResponsiveSection>
+                    <Box 
+                        className='ceiling-space'
+                        display={{base: 'block', lg: 'none'}}
+                        pt={{base: '20vw', lgTablet: '5vw', lg: 0}}
+                    />
 
-                <SectionHeader>Resources</SectionHeader>
-                <Box maxW="1700px"> 
-                    <Title>
-                        Discover helpful resources for your studies and projects.
-                    </Title>
-                </Box>
+                    <SectionHeader>Resources</SectionHeader>
+                    <Box maxW="1700px"> 
+                        <Title>
+                            Discover helpful resources for your studies and projects.
+                        </Title>
+                    </Box>
 
-                <Box>
+                    <Box>
 
-                    <Flex as="ul"
-                        listStyleType="none" p={0} w={{base: "100%", md: "60%", lgTablet: '70%',mdDesktop: '50%', lgDesktop: '100%'}}
-                        justify={{base: "center"}} variant="unstyled"
-                        flexWrap="wrap" gap={{base: 6, lgDesktop: 10, xlDesktop: 16}} mx="auto"
-                        mt={{base: '5rem', lgTablet:'7.5rem', lgDesktop:'wrap2Md'}}
-                        transition="all 300ms ease"
-                    >
-                        {
-                            tabs.map(filter => {
-                                return (
-                                    <Box key={filter}
-                                        cursor="pointer"
-                                        color={filter === selectedTab ? "colorBlue" : "inherit"}
-                                        fontWeight={filter === selectedTab ? "500" : "inherit"}
-                                        borderBottom={filter === selectedTab && "1px"} borderBottomColor={filter === selectedTab  && "colorBlue"}
-                                        onClick={() => {
-                                            setSelectedTab(filter)
-                                            filterResources(filter)
-                                        }}
-                                        transition="all 300ms ease"
-                                        _hover={{
-                                            color: "colorBlue"
-                                        }}
-                                    >
-                                        
-                                        {filter}
-                                        
-                                    </Box>
-                                )
-                            })
-                        }
-                        
-                    </Flex>
-
-                    <TabContent >
-                        {
-                            resourceData.map((resource, index) => {
-                                return (
-                                    <GridItem
-                                        key={resource.id || index}
-                                    >
-                                        <Resource resource={resource} index={index}/>
-                    
-                                    </GridItem>
-                                )
-                            })
+                        <Flex as="ul"
+                            listStyleType="none" p={0} w={{base: "100%", md: "60%", lgTablet: '70%',mdDesktop: '50%', lgDesktop: '100%'}}
+                            justify={{base: "center"}} variant="unstyled"
+                            flexWrap="wrap" gap={{base: 6, lgDesktop: 10, xlDesktop: 16}} mx="auto"
+                            mt={{base: '5rem', lgTablet:'7.5rem', lgDesktop:'wrap2Md'}}
+                            transition="all 300ms ease"
+                        >
+                            {
+                                tabs.map(filter => {
+                                    return (
+                                        <Box key={filter}
+                                            cursor="pointer"
+                                            color={filter === selectedTab ? "colorBlue" : "inherit"}
+                                            fontWeight={filter === selectedTab ? "500" : "inherit"}
+                                            borderBottom={filter === selectedTab && "1px"} borderBottomColor={filter === selectedTab  && "colorBlue"}
+                                            onClick={() => {
+                                                setSelectedTab(filter)
+                                                filterResources(filter)
+                                            }}
+                                            transition="all 300ms ease"
+                                            _hover={{
+                                                color: "colorBlue"
+                                            }}
+                                        >
+                                            
+                                            {filter}
+                                            
+                                        </Box>
+                                    )
+                                })
+                            }
                             
-                        }
-                        
-                    </TabContent>
-                </Box>
+                        </Flex>
 
-                <Box 
-                    className='floor-space'
-                    display={{base: 'block', lg: 'none'}}
-                    pt={{base: '20vw', lgTablet: '5vw', lg: 0}}
-                />
-                
-            </ResponsiveSection>
+                        <TabContent >
+                            {
+                                resourceData.map((resource, index) => {
+                                    return (
+                                        <GridItem
+                                            key={resource.id || index}
+                                        >
+                                            <Resource resource={resource} index={index}/>
+                        
+                                        </GridItem>
+                                    )
+                                })
+                                
+                            }
+                            
+                        </TabContent>
+                    </Box>
+
+                    <Box 
+                        className='floor-space'
+                        display={{base: 'block', lg: 'none'}}
+                        pt={{base: '20vw', lgTablet: '5vw', lg: 0}}
+                    />
+                    
+                </ResponsiveSection>
+            </HeroAnimationWrapper>
         </Layout>
     )
 }
